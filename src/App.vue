ac26546501a86d801aa17722bc8204dcf22b3ed1
STACIONARAS\Tadas <tadaskupr@gmail.com>
2021-01-27 15:43:53 +0200

    Added navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
-    <router-view/>
+    <Header></Header>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+<style lang="scss" scoped>
 #app {
   font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  color:#000000;
 }
 </style>
