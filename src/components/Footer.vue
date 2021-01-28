819d0ce3af5068ce81ac4ed7ac1c1f462a6cf568
TadasKu <50514202+TadasKu@users.noreply.github.com>
2021-01-28 09:12:38 +0200

    Added footer (#6)

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="footer">
+    <b-container class="fixed-bottom">
+        <label>SCodingas Task</label>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style scoped>
+label {
+  font-weight: bold;
+  font-size: medium;
+  color: #fff;
+  margin: 1rem;
+}
+.fixed-bottom {
+  background-color: #343a40;
+  border: 1px solid;
+  border-top-left-radius: 2em;
+  border-top-right-radius: 2em;
+  border-bottom: none;
+}
+</style>
